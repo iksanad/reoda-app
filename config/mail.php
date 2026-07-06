d0424ba08b9ff5c6aa37d536f6b348b768b554ca
Iksan Arya <iksanarya123@gmail.com>
2026-07-06 15:20:58 +0700

    feat: add resend email driver

--- config/mail.php
+++ config/mail.php
@@ -57,6 +57,10 @@ return [
             'transport' => 'postmark',
         ],
 
+        'resend' => [
+            'transport' => 'resend',
+        ],
+
         'sendmail' => [
             'transport' => 'sendmail',
             'path' => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -bs -i'),
